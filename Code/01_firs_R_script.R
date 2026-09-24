2609d7feee9be353b25a0a25f9d270352b3f8fea
juliusoldehaver <oldehaver48@gmail.com>
2026-09-24 12:31:26 +0200

    Create 01_firs_R_script.R

---
+++ Code/01_firs_R_script.R
@@ -0,0 +1,14 @@
+first operation
+# first operation
+2 + 3
+
+# first object
+cecilia <- 2 + 3
+
+# second object
+julius <- 4 + 6
+
+# additional operations
+cecilia + julius # 2 + 3 + 4 + 6
+cecilia * julius # (2 + 3) * (4 + 6)
+cecilia ^ julius # (2 + 3) ^ (4 + 6)
